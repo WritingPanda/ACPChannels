90ab9bf946e7fce94443bdb233aef42cd1c474eb
Omar Quimbaya <omar.quimbaya4@gmail.com>
2017-04-06 10:51:40 -0500

    Moving routing.py file to the right place. Adding *.pptx to .gitignore because I don't need that in the repo. I only need the PDF.

--- ACPChannels/routing.py
+++
@@ -1,8 +0,0 @@
-from channels.routing import route
-from presentation.consumers import ws_message
-
-
-channel_routing = [
-    # route("http.request", "presentation.consumers.http_consumer"),
-    route("websocket.receive", ws_message),
-]
--- presentation/routing.py
+++ presentation/routing.py
@@ -1,6 +1,8 @@
 from channels.routing import route
+from presentation.consumers import ws_message
 
 
 channel_routing = [
-    route("http.request", "presentation.consumers.http_consumer"),
+    # route("http.request", "presentation.consumers.http_consumer"),
+    route("websocket.receive", ws_message),
 ]
